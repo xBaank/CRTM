bc4f08e6f0bf7f70114246b3ee4027831d0d012e
Roberto Blázquez <elroby444@gmail.com>
2022-12-04 00:51:59 +0100

    Refactor

--- src/main/kotlin/CRTMClient.kt
+++ src/main/kotlin/CRTMClient.kt
@@ -4,6 +4,8 @@ import okhttp3.OkHttpClient
 import stops.StopsClient
 import java.util.concurrent.TimeUnit
 
+const val CRTM_URL = "https://www.crtm.es/widgets/api"
+
 class CRTMClient(
     httpClient: OkHttpClient = OkHttpClient.Builder()
         .readTimeout(30, TimeUnit.SECONDS)
--- src/main/kotlin/Constants.kt
+++
@@ -1,1 +0,0 @@
-const val CRTM_URL = "https://www.crtm.es/widgets/api"
