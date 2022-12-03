50605dacaf96ec02a2c10261e87871b038a85d12
Roberto Blázquez <elroby444@gmail.com>
2022-12-03 23:53:28 +0100

    Specify return type

--- src/main/kotlin/extensions/StringExtensions.kt
+++ src/main/kotlin/extensions/StringExtensions.kt
@@ -3,4 +3,4 @@ package extensions
 import java.net.URLEncoder
 import java.nio.charset.Charset
 
-fun Any.encode(charset: Charset = Charsets.UTF_8) = URLEncoder.encode(this.toString(), charset)
+fun Any.encode(charset: Charset = Charsets.UTF_8): String = URLEncoder.encode(this.toString(), charset)
